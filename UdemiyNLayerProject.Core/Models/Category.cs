393454f9e1897e9078d4c24ef2edac13265c2ea1
alieryaman <postauyelik@gmail.com>
2020-03-21 23:46:28 +0300

    Add project files.

---
+++ UdemiyNLayerProject.Core/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace UdemiyNLayerProject.Core.Models
+{
+    class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ UdemiyNLayerProject.Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemiyNLayerProject.Core.Models
+{
+    class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+
+        public virtual Category Category{ get; set; }
+
+
+
+
+    }
+}
