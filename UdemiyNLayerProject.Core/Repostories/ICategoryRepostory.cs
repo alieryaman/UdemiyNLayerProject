2a0530ceaa9b662c1f6dbde104c97b7fc236a4c5
alieryaman <postauyelik@gmail.com>
2020-03-22 00:41:41 +0300

    IService oluşturuldu

---
+++ UdemiyNLayerProject.Core/Repostories/ICategoryRepostory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemiyNLayerProject.Core.Models;
+
+namespace UdemiyNLayerProject.Core.Repostories
+{
+     interface  ICategoryRepostory:IRepostory<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int CategoryId);
+
+
+    }
+}
---
+++ UdemiyNLayerProject.Core/Repostories/IProductRepostory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemiyNLayerProject.Core.Models;
+
+namespace UdemiyNLayerProject.Core.Repostories
+{
+    interface IProductRepostory
+    {
+
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
