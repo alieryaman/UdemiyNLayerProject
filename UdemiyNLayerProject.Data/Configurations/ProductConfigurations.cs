395a81e781a9f2deac423607c20ad83372a1f103
alieryaman <postauyelik@gmail.com>
2020-03-22 11:37:02 +0300

    ProductConfigurations

---
+++ UdemiyNLayerProject.Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using UdemiyNLayerProject.Core.Models;
+
+namespace UdemiyNLayerProject.Data.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x=>x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Id).IsRequired().HasColumnType("decimal(18.2)");
+            builder.Property(x => x.InnerBarcode).HasMaxLength(50);
+
+        }
+    }
+}
